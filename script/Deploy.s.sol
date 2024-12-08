f5728ad568d992f872337af3138aa2a76c5c8961
Pdevu616 <pranith616@gmail.com>
2024-12-08 16:09:50 -0500

    initial commit

---
+++ script/Deploy.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import "../src/Midterm.sol";
+
+contract Deploy is Script {
+    Midterm public midterm;
+    Midterm.Book public book;
+
+    function setUp() public {
+        vm.broadcast();
+
+        midterm = new Midterm("Programming Foundry", "Ellan", 100);
+    }
+
+    function run() public {
+        log_book();
+    }
+
+    function log_book() internal{
+        book = midterm.get_book();
+        console.log("Author :", book.author);
+    }
+}
---
+++ test/Midterm.t.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.26;
+
+import {Test, console} from "forge-std/test.sol";
+import "../src/Midterm.sol";
+
+contract MidtermTest is Test{
+
+    Midterm midterm;
+    Midterm.Book public book;
+
+
+    function setUp() public {
+        midterm = new Midterm("Programming Foundry", "Ellan", 100);
+        console.log("Midterm contract Deployed at:",address(midterm));
+    }
+
+    function test_true() public pure{
+        assert(true);
+    }
+
+    function test_get_book() public{
+        book=midterm.get_book();
+        assertEq(book.pages,100);
+        assertEq(book.author,"Ellan");
+        assertEq(book.title,"Programming Foundry");
+    }
+
+    function test_update_pages() public{
+        midterm.update_pages(200);
+        book= midterm.get_book();
+        assertEq(book.pages,200);
+    }
+}
